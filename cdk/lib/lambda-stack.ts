66cbf7594dde006ce504f9cd1c7c53631d83b60c
Don McGarry <donmcg@amazon.com>
2021-02-18 22:18:18 -0500

    Add DDB Policy to lambda functions IAM Role

--- cdk/lib/lambda-stack.ts
+++ cdk/lib/lambda-stack.ts
@@ -2,6 +2,7 @@ import * as codedeploy from '@aws-cdk/aws-codedeploy';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as events from '@aws-cdk/aws-events';
+import * as iam from '@aws-cdk/aws-iam';
 import * as targets from '@aws-cdk/aws-events-targets';
 
 import { App, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
@@ -28,6 +29,53 @@ export class LambdaStack extends Stack {
       runtime: lambda.Runtime.PYTHON_3_8,
       description: `Function generated on: ${new Date().toISOString()}`,
     });
+
+
+
+    const lambdaDDBTablePolicy = new iam.PolicyStatement({
+      actions:[
+        "dynamodb:BatchGetItem",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:UpdateTimeToLive",
+        "dynamodb:ConditionCheckItem",
+        "dynamodb:UntagResource",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:PartiQLUpdate",
+        "dynamodb:Scan",
+        "dynamodb:ListTagsOfResource",
+        "dynamodb:Query",
+        "dynamodb:UpdateItem",
+        "dynamodb:DescribeTimeToLive",
+        "dynamodb:TagResource",
+        "dynamodb:PartiQLSelect",
+        "dynamodb:DescribeTable",
+        "dynamodb:PartiQLInsert",
+        "dynamodb:GetItem",
+        "dynamodb:UpdateTable",
+        "dynamodb:PartiQLDelete",
+        "dynamodb:DescribeTableReplicaAutoScaling"],
+      resources:[
+        ddbtable.tableArn,
+        ddbtable.tableArn + "/index/*"
+      ]
+    });
+
+    func.addToRolePolicy(lambdaDDBTablePolicy);
+
+    const lambdaDDBPolicy = new iam.PolicyStatement({
+      actions:[
+        "dynamodb:DescribeReservedCapacityOfferings",
+        "dynamodb:ListTables",
+        "dynamodb:DescribeReservedCapacity",
+        "dynamodb:DescribeLimits"
+      ],
+      resources: [
+        "*"
+      ]
+    });
+
+    func.addToRolePolicy(lambdaDDBPolicy);
 
     const eventrule = new events.Rule(this, 'rss-lambda-cron-rule', {
       schedule: events.Schedule.expression('rate(5 minutes)')
