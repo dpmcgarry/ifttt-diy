066682c66be56fd7c5a94f6cfbfe71b9210e7483
Don McGarry <donmcg@amazon.com>
2021-02-20 22:44:43 -0500

    Need my settings for the pipeline to work (duh)

---
+++ cdk/bin/ifttt-diy-settings.ts
@@ -0,0 +1,7 @@
+export class IFTTTDIYSettings{
+    static readonly githubSecretName: 'dpmcgarry-github-oauth';
+    static readonly githubOwner: 'dpmcgarry';
+    static readonly githubRepoName: 'ifttt-diy';
+    static readonly githubBranchName: 'main';
+    static readonly rssDDBTableName: 'ifttt-diy-rssfeeds';
+}
